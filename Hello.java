a1eba28e63be26d9dc2166b9cd96aa5608c011ed
Wkreiner <wkreiner@email.arizona.edu>
2017-09-11 16:45:45 -0700

    my side finished

--- Hello.java
+++ Hello.java
@@ -9,24 +9,24 @@ public class Hello {
     }
 
     public static void callSign() {
-        
-        // Red Leader should add a print statement that says "Lock S-foils in attack position." 
-	
+
+        // Red Leader should add a print statement that says "Lock S-foils in attack position."
+
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
     }
 
     public static void goldLeader() {
-
+      System.out.println("Loosen up!");
         // Gold Leader should add a print statement that says "Loosen up!"
-   
+
 
     }
 
     public static void redLeader() {
- 	System.out.println("I copy, gold leader!")
+ 	System.out.println("I copy, gold leader!");
         // Red Leader should add a print statement that says "I copy, gold leader!"
-        
+
 
     }
 
